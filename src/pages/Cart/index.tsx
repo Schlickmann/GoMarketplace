661fe51367e482825743b049a6a4def046ba48e3
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-07-01 17:03:19 -0700

    displaying total in cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,15 +47,23 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, product) => {
+      const productSubtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return acc + productSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, product) => {
+      const productQuantity = product.quantity;
 
-    return 0;
+      return acc + productQuantity;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
